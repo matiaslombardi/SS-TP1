1ffebff75667f0e16c6e2bd84b19f50bc32cf307
Matías Federico Lombardi <mflombardi2000@gmail.com>
2022-08-11 20:50:46 -0300

    fix: minor fix
    Co-authored-by: srosati <srosati@itba.edu.ar> Co-authored-by: patrickmdey <patrickmdey@hotmail.com>

--- graphics/main.py
+++ graphics/main.py
@@ -7,7 +7,7 @@ with open("../src/main/resources/static1.txt", "r") as static_file:
     space_size = float(static_file.readline())
     for idx in range(particle_count):
         [radius, _] = [float(n) for n in static_file.readline().split()]
-        particles.append({"id": idx + 1, "radius": radius})
+        particles.append({"id": idx, "radius": radius})
     static_file.close()
 
 with open("../src/main/resources/dynamic1.txt", "r") as dynamic_file:
@@ -32,7 +32,6 @@ with open("../out.txt", "r") as out_file:
     out_file.close()
 
 selected = int(input("Enter a number: "))
-print(particles[selected]["neighbours"]) # [99, 100, 69, 41, 76, 13, 16, 81, 19, 83, 84, 22, 25, 91, 28, 93, 63]
 with open("ovito.txt", "w") as ovito_file:
     ovito_file.write(str(particle_count) + '\n')
     ovito_file.write("Title\n")
@@ -40,7 +39,7 @@ with open("ovito.txt", "w") as ovito_file:
         category = "NN"
         if particle["id"] == selected:
             category = "S"
-        elif particle["id"] in particles[selected - 1]["neighbours"]:
+        elif particle["id"] in particles[selected]["neighbours"]:
             category = "N"
         ovito_file.write(f"{particle['radius']} {particle['x']} {particle['y']} {category}\n")
     ovito_file.close()
