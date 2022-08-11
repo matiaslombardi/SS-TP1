3a0e54e3a039a8a24c62a6e2a3357f658cd5c862
Matías Federico Lombardi <mflombardi2000@gmail.com>
2022-08-11 20:41:48 -0300

    fix: minor fix
    Co-authored-by: srosati <srosati@itba.edu.ar> Co-authored-by: patrickmdey <patrickmdey@hotmail.com>

--- graphics/main.py
+++ graphics/main.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 particles = []
-with open("../src/main/resources/static.txt", "r") as static_file:
+with open("../src/main/resources/static1.txt", "r") as static_file:
     particle_count = int(static_file.readline())
     space_size = float(static_file.readline())
     for idx in range(particle_count):
@@ -10,7 +10,7 @@ with open("../src/main/resources/static.txt", "r") as static_file:
         particles.append({"id": idx + 1, "radius": radius})
     static_file.close()
 
-with open("../src/main/resources/dynamic.txt", "r") as dynamic_file:
+with open("../src/main/resources/dynamic1.txt", "r") as dynamic_file:
     dynamic_file.readline()
     for idx in range(particle_count):
         [x, y] = [float(n) for n in dynamic_file.readline().split()]
@@ -23,6 +23,7 @@ with open("../out.txt", "r") as out_file:
     for idx in range(particle_count):
         parts = [int(n) for n in out_file.readline().split()]
         if len(parts) == 1:
+            particles[idx]["neighbours"] = []
             continue
 
         neighbours = parts[1:]
@@ -30,11 +31,11 @@ with open("../out.txt", "r") as out_file:
 
     out_file.close()
 
-print(particles[28]["neighbours"]) # [99, 100, 69, 41, 76, 13, 16, 81, 19, 83, 84, 22, 25, 91, 28, 93, 63]
+selected = int(input("Enter a number: "))
+print(particles[selected]["neighbours"]) # [99, 100, 69, 41, 76, 13, 16, 81, 19, 83, 84, 22, 25, 91, 28, 93, 63]
 with open("ovito.txt", "w") as ovito_file:
     ovito_file.write(str(particle_count) + '\n')
     ovito_file.write("Title\n")
-    selected = 29
     for particle in particles:
         category = "NN"
         if particle["id"] == selected:
